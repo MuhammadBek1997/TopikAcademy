4cdc7dff00bddf6d7f4d0aaf19d55a35c7ea14fb
botir <botirberdaliyev@gmail.com>
2026-01-20 15:50:47 +0500

    footer

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,8 +1,37 @@
-import '../style/Footer.css'
+import "../style/Footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="footer">
+      <div className="footer-map">
+        <img src="/images/xarita.png" alt="Map" />
+      </div>
+      <div className="footer-content">
+        <div className="footer-logo">
+          <img src="/images/topic.png" alt="Topik Academy" />
+        </div>
+        <div className="footer-menu">
+          <li>
+            <a href="#">Nega biz</a>
+          </li>
+          <li>
+            <a href="#">Natijalar</a>
+          </li>
+          <li>
+            <a href="#">Kurslar</a>
+          </li>
+          <li>
+            <a href="#">Ko‘p beriladigan savollar</a>
+          </li>
+        </div>
+        <div className="footer-social">
+          <img src="/images/telfon.png" alt="" />
+          <img src="/images/telegram.svg" alt="" />
+          <img src="/images/instagram.svg" alt="" />
+          <img src="/images/youtube.svg" alt="" />
+        </div>
+      </div>
+    </footer>
   )
 }
 
